1e74d54bed29a8228ecd10fcdde7a43991f6369d
amanp <amanprem374@gmail.com>
2024-10-15 09:26:34 +0530

    Add files via Upload

---
+++ loops.js
@@ -0,0 +1,21 @@
+//while loop
+// let i=0;
+// while(i<=10){
+//     console.log(i);
+//     i++;
+// }
+// let total = 0;
+// var i =0;
+// while(i<=10){
+//     total = total+i;
+//     i++;
+// }
+// console.log(total);
+
+
+//for loop
+let total = 0;
+for(let i=0;i<=10;i++){
+    total = total+i;
+}
+console.log("sum of value of i is",total);
