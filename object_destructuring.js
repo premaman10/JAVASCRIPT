c2e3b79d553357d0c9ddd5176f006ca7128ac43e
amanp <amanprem374@gmail.com>
2024-10-28 10:00:00 +0530

    Add files via Upload

--- object_destructuring.js
+++ object_destructuring.js
@@ -1,6 +1,35 @@
 const band = {
     bandname: "The Local Train",
     famoussong:"Choo Lo",
+    year: 2004,
 };
-const bandname = band.bandname;
-const famoussong = band.famoussong;
+// const v1 = band.bandname;
+// const v2 = band.famoussong;
+// console.log(v1,v2);
+const{bandname, famoussong,year:v3}=band;
+console.log(bandname);
+console.log(famoussong);
+console.log(v3);
+
+//object inside array
+const arr = [{
+    name: "John",
+    age: 25,
+    gender: "male",
+},{
+    name: "Ram",
+    age: 30,
+    gender: "male",
+},{
+    name:"Sita",
+    age :25,
+    gender: "female",
+}]
+for(let user of arr){
+    console.log(user);
+    console.log(user.name);
+    console.log(user.age)
+    
+}
+const[{name},{age},{gender}]=user1;
+console.log(user1);
