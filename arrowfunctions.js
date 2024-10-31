77f15f3062b2f621829282a8fde789b6c58da1ba
amanp <amanprem374@gmail.com>
2024-10-31 10:00:00 +0530

    Add files via Upload

--- arrowfunctions.js
+++ arrowfunctions.js
@@ -22,3 +22,43 @@ console.log(firstchar("hello"));
 // function hello(){
 //     console.log("hello");
 // }
+console.log(hi);//it will result as undefined
+var hi = "hello";
+
+// console.log(h);//uncaught referenceerror
+// const h = "hii";
+
+
+//funct inside funct
+
+const app =()=>{
+    const greet = () => {
+        console.log("greeting");
+    }
+    const add = (n1,n2)=>{
+        console.log(n1+n2);
+    }
+    greet();
+    add(2,4);
+}
+app();
+
+
+//lexical scope
+function myapp(){
+    const myvar = "hii";
+    function myfun(){
+        const myvar=59;
+        console.log("inside myfun",myvar);
+    }
+    const fun2 = ()=>{
+        console.log("myvarianle is "+myvar);
+    }
+    const fun3 = function(){
+        
+    }
+    myfun();
+    fun2();
+    console.log(myvar);
+}
+myapp();
