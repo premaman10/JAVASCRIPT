b70832df9c3e388de111ab6363be30a304aa64f6
amanp <amanprem374@gmail.com>
2024-10-31 10:00:00 +0530

    Add files via Upload

---
+++ arrowfunctions.js
@@ -0,0 +1,24 @@
+// console.log("testing");
+const happybirthday = ()=>{
+    console.log("<<<<<<HAPPY BIRTHDAY>>>>>>")
+}
+happybirthday();
+const sumthreenum = (a,b,c)=>{
+    console.log(a+b+c); 
+}
+sumthreenum(1,2,3);
+
+const isEven = (num)=>{
+        console.log(num%2==0);
+}
+isEven(8);
+
+const firstchar = anystring => anystring[0];
+console.log(firstchar("hello"));
+
+
+//HOISTING
+// hello();
+// function hello(){
+//     console.log("hello");
+// }
