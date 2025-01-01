280856fa98369a69ad863a0dee698dd6bd173586
amanp <amanprem374@gmail.com>
2025-01-01 10:45:54 +0530

    Add files via Upload

--- const.js
+++ const.js
@@ -1,2 +1,3 @@
 const pi = 3.14;
 console.log(pi*2);
+//we have to use const when we have a fixed value of the variable
--- var.js
+++ var.js
@@ -1,4 +1,4 @@
-"use strict"
+" use strict"
 var firstname = "Prem Aman";
 console.log(firstname);
 firstname="Anu";
